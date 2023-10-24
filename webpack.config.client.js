5e678b05292f13c6e4edb6490470a3a875f8983d
Ahmad Afshari <46311202+ahmadaf94@users.noreply.github.com>
2023-10-24 10:23:15 +0200

    fix: fix ts and webpack configuration errors

--- webpack.config.client.js
+++ webpack.config.client.js
@@ -12,7 +12,7 @@ module.exports = {
 	output: {
 		path: path.resolve(__dirname + "/dist/static"),
 		filename: "[name].[contenthash].js",
-		publicPath: "",
+		publicPath: "/",
 	},
 	resolve: {
 		extensions: [".ts", ".tsx", ".js"],
